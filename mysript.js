b18a5e42b8c36f7fcdc5bac51b7b17ec5a2a5d37
Wahab Rehman <wahab_rehman1256@outlook.com>
2020-07-24 22:40:00 +0100

    Removing solution

--- mysript.js
+++ mysript.js
@@ -1,71 +1,44 @@
-
 let randomNumber = Math.floor((Math.random() * 100) + 1);
-let Playing = true;
-let gameOver = false;
-let Tries = 7;
-let TriesTaken = 0;
-let numberOfTries;
+
 
 
 function guessNumber() {
 
-	if (Playing) {
-
+		//Collect input from the user
 		let guess = document.querySelector('.inputs-Values').value;
 
-		//bad inputs
-		if (guess === 0 || guess === '' || guess > 100 || guess < 0) {
+		//If the user inputs a bad input ie 0, empty string, number greater that 100, number less than zero Print "Please enter a number between 1 and 100"
+	
+		//If the users guess is higher than the random number print Number is too high, try again (hint use .final-out class to print)
 
-			document.querySelector('.final-output').textContent = 'Please enter a number between 1 and 100';
+		//If the users guess is lower than the random number print Number is too low, try again  (hint use .final-out class to print)
 
-			//guess is to high
-		} else if (guess > randomNumber) {
+		//If the user has guessed the random number correctly print out the randomnumber with a message "is correct, You Win!!!"
+	
+		//Extension task
+		//Develop the application further and display the number of tries taken for the user to guess the number
+		//If the user guesses the random number within the number of tries, print 'It took you ' + TriesTaken + ' tries';
 
-			Tries -= 1;
-			TriesTaken += 1;
-			document.querySelector('.final-output').textContent = 'Number is too high, try again';
-			numberOfTries = document.querySelector('.Tries-output').textContent = 'Number of Tries: ' + Tries;
-
-			//guess is to low
-		} else if (guess < randomNumber) {
-
-			Tries -= 1;
-			TriesTaken += 1;
-			document.querySelector('.final-output').textContent = 'Number is too low, try again';
-			numberOfTries = document.querySelector('.Tries-output').textContent = 'Number of Tries: ' + Tries;
-
-			//guess is correct
-		} else {
-
-			Tries -= 1;
-			TriesTaken += 1;
-			document.querySelector('.final-output').textContent = guess + ' is correct, You Win!!!';
-			numberOfTries = document.querySelector('.Tries-output').textContent = 'It took you ' + TriesTaken + ' tries';
-			Playing = gameOver;
-		}
-
-		//out of tries
-		if (Tries === 0 && guess != randomNumber) {
-
-			numberOfTries = numberOfTries = document.querySelector('.Tries-output').textContent = 'You Lose, the number was ' + randomNumber;
-			document.querySelector('.final-output').textContent = '';
-			Playing = gameOver;
-
-		}
-	}
-
+		//Extension task
+		//Lets make our application a little more cooler, incorporate a tries functionality, where a user only has certain number of tries to guess the number
+		//If the user were to exeed the number of tries the print out a message print 'You Lose, the number was ' + randomNumber;
+	
+	
 }
 
-//new game function
+//For this task we will be making newGame button function which will reset our game, once the user clicks on thos button the user will have new random number to guess
+//1. reset the values inside the body of the function
+//2. Attach our new game button using an evenlistener to the .btnNewGame button
 function newGame() {
 
-	Tries = 7;
-	TriesTaken = 0;
-	randomNumber = Math.floor((Math.random() * 100) + 1);
-	guess = document.querySelector('.inputs-Values').value = '';
-	numberOfTries = document.querySelector('.Tries-output').textContent = 'Number of Tries: ' + Tries;
-	document.querySelector('.final-output').textContent = 'Get a number between 1 and 100';
-	Playing = true;
+	//Your code here
+	//Reset randomNumber
+	//Reset users input feild
+
+	//Extention 
+	//Reset tries, and triesTaken by the user
+
+
 }
 
 //keyboard exception
@@ -79,5 +52,4 @@ function keyBoardEvents(e) {
 
 
 document.querySelector('.btnGuess').addEventListener('click', guessNumber);
-document.querySelector('.btnNewGame').addEventListener('click', newGame);
 document.addEventListener('keypress', keyBoardEvents);
